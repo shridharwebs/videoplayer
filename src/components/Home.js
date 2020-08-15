10734952c0eebbd7046cab03452b287e6947bc42
shridharwebs <spwebs3@gmail.com>
2020-08-15 21:02:24 +0530

    updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,16 +4,20 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div className="container">
-      <ul class="list-group">
-        <li class="list-group-item border-0 d-flex justify-content-between align-items-center">
-          <Link to="/reactjs">React js course</Link>
-          <span class="badge badge-primary badge-pill">14</span>
-        </li>
-        <li class="list-group-item border-0 d-flex justify-content-between align-items-center">
-          <Link to="/nodejs">Node js course</Link>
-          <span class="badge badge-primary badge-pill">2</span>
-        </li>
-      </ul>
+      <div className="col-md-6">
+        <br />
+        <br />
+        <ul class="list-group">
+          <li class="list-group-item border-0 d-flex justify-content-between align-items-center">
+            <Link to="/reactjs">React js course</Link>
+            <span class="badge badge-primary badge-pill">14</span>
+          </li>
+          <li class="list-group-item border-0 d-flex justify-content-between align-items-center">
+            <Link to="/nodejs">Node js course</Link>
+            <span class="badge badge-primary badge-pill">2</span>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
